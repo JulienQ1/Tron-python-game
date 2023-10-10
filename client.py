75ae154ee314f1b51523604d05b2c8b5f827573e
Infinity0901 <antoinem9@icloud.com>
2023-10-10 14:27:48 +0200

    h

---
+++ client.py
@@ -0,0 +1,82 @@
+import socket
+import pygame
+from pygame.locals import *
+
+# Initialisation du client
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1", 12345))  # Remplacez par l'adresse IP et le port du serveur
+
+# Initialisation de Pygame
+pygame.init()
+screen_width, screen_height = 700, 650
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+# Couleurs
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+startGame = False
+while startGame == False:
+    screen.fill(BLACK)
+    font = pygame.font.Font('freesansbold.ttf', 60)
+    font2 = pygame.font.Font('freesansbold.ttf', 36)
+    startLabel = font.render('Tron Game - CreaTech', 1, (WHITE))
+    label2 = font2.render('Press SHIFT to start!', 1, (WHITE))
+    for event in pygame.event.get():
+        keyState = pygame.key.get_pressed()
+        if keyState[pygame.K_RSHIFT] or keyState[pygame.K_LSHIFT]:
+            startGame = True
+        screen.blit(startLabel, (65, 225))
+        screen.blit(label2, (170, 450))
+        pygame.display.flip()
+
+
+
+# Position initiale du carré contrôlé par le client
+x, y = 200, 200
+
+# Taille du carré
+square_size = 50
+
+# Boucle principale du jeu
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    # Gestion des mouvements du carré par les touches fléchées
+    keys = pygame.key.get_pressed()
+    if keys[K_LEFT]:
+        x -= 5
+    if keys[K_RIGHT]:
+        x += 5
+    if keys[K_UP]:
+        y -= 5
+    if keys[K_DOWN]:
+        y += 5
+
+    # Limiter les coordonnées pour éviter le clipping
+    x = max(0, min(x, screen_width - square_size))
+    y = max(0, min(y, screen_height - square_size))
+
+    # Dessiner le carré
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, WHITE, (x, y, square_size, square_size))
+
+    # Envoyer la position du carré au serveur
+    client.send(f"{x},{y}".encode())
+
+    # Recevoir les positions de l'autre carré depuis le serveur
+    data = client.recv(1024)
+    if data:
+        other_x, other_y = map(int, data.decode().split(','))
+        # Dessiner l'autre carré
+        pygame.draw.rect(screen, WHITE, (other_x, other_y, square_size, square_size))
+
+    pygame.display.flip()
+    clock.tick(30)
+
+pygame.quit()
+client.close()
--- server.py
+++ server.py
@@ -1,7 +1,42 @@
 import socket
 import threading
-import pygame
 
-hop =2
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("0.0.0.0", 12345))  
+server.listen(2)  
+clients = []
+
+
+def handle_client(client_socket):
+    while True:
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+
+            broadcast(data, client_socket)
+
+        except Exception as e:
+            print(f"Erreur: {str(e)}")
+            break
+
+    client_socket.close()
+
+
+def broadcast(message, client_socket):
+    for client in clients:
+        if client != client_socket:
+            try:
+                client.send(message)
+            except Exception as e:
+                print(f"Erreur lors de la diffusion: {str(e)}")
+
+
+while True:
+    client_socket, addr = server.accept()
+    clients.append(client_socket)
+
+    client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+    client_handler.start()
 
 
