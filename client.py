4cfada61b77d452194a41b6534c50e81ae0ea083
ElessarWillomoont <xynmpsy7ihm5mv22ode@outlook.com>
2023-10-10 10:50:23 +0200

    previous version cause some bug with estabilsh and close socket, fix them this time

--- client.py
+++ client.py
@@ -3,9 +3,9 @@ import socket
 import time #to count and control the time of a loop
 
 SERVER_IP = "127.0.0.1" #The IP of the server
-SERVER_PORT = "0000" #The port used of server
+SERVER_PORT = 0000 #The port used of server
 CLIENT_IP = "127.0.0.1" #The IP of client
-CLIENT_PORT = "0000" #The port used by client
+CLIENT_PORT = 0000 #The port used by client
 LOGIN_SUC = "login_success" #The message given by server that means username is right
 LOGIN_FAIL = "login_fail"
 FPS = 60 #how many loops every second
@@ -13,18 +13,16 @@ USER_NUMBER = 4
 START = "game_start" #start signal provide by server
 STOP = "game_stop"#stop signal provide by server
 
-# make an object of socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    # connect the server
+# make an object of socket,and connect
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (SERVER_IP, SERVER_PORT)
-s.connect(server_address)
+client_socket.connect(server_address)
 
 #login procedure
 print("Welcom to our game")
 while True:
     username = input("Your user name is \n")
-    result = login(username,s)
+    result = login(username,client_socket)
     # if login successfully, go to main loop, else stay here
     if result == "suc":
         break
@@ -36,32 +34,24 @@ while True:
 #wait for the game to start
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Connect to server
-server_address = (SERVER_IP, SERVER_PORT)
-client_socket.connect(server_address)
+while True:
+    # Getting data from server
+    data = client_socket.recv(1024).decode('utf-8')
+    
+    if data == START:
+        print("Game will start")
+        break
+    else:
+        print("wait for other users")
+        time.sleep(1)
 
-try:
-    while True:
-        # Getting data from server
-        data = client_socket.recv(1024).decode('utf-8')
-        
-        if data == START:
-            print("Game will start")
-            break
-        else:
-            print("wait for other users")
-            time.sleep(1)
-
-finally:
-    client_socket.close()
 
 
 #main loop below
 while True:
     start_time = time.time() # Record the time of the loop start
     #put the main boday of our game below
-
+    
 
     #below and first line for make sure loop FPS time every second.
     passed_time = time.time() - start_time
