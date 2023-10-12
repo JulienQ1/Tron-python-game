62c97675e7e2b917e2549912737100e461b87a6d
ElessarWillomoont <xynmpsy7ihm5mv22ode@outlook.com>
2023-10-12 12:37:26 +0200

    new problem

--- server/server.py
+++ server/server.py
@@ -10,7 +10,7 @@ LOGIN_SUC = "login_success"
 LOGIN_FAIL = "login_fail"
 PLAYER_CODES = ["A", "B", "C", "D"]
 GAME_START = "game_start"
-SPEED = 3
+SPEED = 1
 
 # Create a socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -69,7 +69,7 @@ while True:
                     client_socket.sendall(GAME_START.encode('utf-8'))
 
                     while True:
-                        client_socket.settimeout(0.1)
+                        client_socket.settimeout(0.01)
                         try:
                             move_command = client_socket.recv(1024).decode('utf-8').split(',')
                             player_code, direction = move_command[0], move_command[1]
@@ -83,6 +83,13 @@ while True:
                                 x -= GRID_SIZE
                             elif direction == "right" and x < 670:
                                 x += GRID_SIZE
+
+                            # Check if the grid cell is used
+                            grid_x, grid_y = (x - 100) // GRID_SIZE, y // GRID_SIZE
+                            if game_grid[grid_y][grid_x] == "used":
+                                client_socket.sendall(f"{player_code},loss".encode('utf-8'))
+                                #break  # Exit the loop, ending the game for this player
+
                             game_grid[grid_y][grid_x] = "used"
                             new_place[player_code] = (x,y)
                             player_positions[player_code] = (x, y)
@@ -90,19 +97,25 @@ while True:
                             delta_y = new_place[player_code][1] - old_place[player_code][1]
                             delta_place[player_code] = (delta_x, delta_y)
                         except socket.timeout:
-                            print("No data received after 0.1 seconds, moving on...")
+                            #print("No data received after 0.1 seconds, moving on...")
                             for player_code in PLAYER_CODES:
                                 x0,y0 = player_positions[player_code]
                                 delta_x,delta_y = delta_place[player_code]
                                 x1 = x0 + delta_x*SPEED
                                 y1 = y0 + delta_y*SPEED
                                 player_positions[player_code] = (x1,y1)
-                            # Check if the grid cell is used
-                        grid_x, grid_y = (player_positions[player_code][0] - 100) // GRID_SIZE, player_positions[player_code][1] // GRID_SIZE
-                        if game_grid[grid_y][grid_x] == "used" or grid_x >= 670 or grid_x <= 90 or grid_y<= 0 or grid_y >= 580:
-                            client_socket.sendall(f"{player_code},loss".encode('utf-8'))
-                            #break  # Exit the loop, ending the game for this player
-
+                                #print(x1,x0,y1,y0)
+                                if (delta_x !=0 or delta_y != 0):
+                                    #print('not startimg point colliding')
+                                    grid_x_delta, grid_y_delta = (x1 - 90) // GRID_SIZE, y1 // GRID_SIZE
+                                    if game_grid[grid_y_delta][grid_x_delta] == "used":
+                                        print('collided')
+                                        client_socket.sendall(f"{player_code},loss".encode('utf-8'))
+                                        print("loss send at",game_grid[grid_y_delta][grid_x_delta],(x1-90),y1)
+                                        #break  # Exit the loop, ending the game for this player
+                                    else:
+                                        print('used case now')
+                                        game_grid[grid_y_delta][grid_x_delta] = "used"
                         positions = ','.join([f"{code}:{x}-{y}" for code, (x, y) in player_positions.items()])
                         client_socket.sendall(positions.encode('utf-8'))
             else:
