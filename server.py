94169e9b7f0bd8648263491e6a3de6810acb2b5a
Infinity0901 <antoinem9@icloud.com>
2023-10-12 15:39:54 +0200

    fin couleurs

--- server.py
+++ server.py
@@ -2,12 +2,13 @@ import socket
 import threading
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("0.0.0.0", 12345))  
-server.listen(2)  
+server.bind(("0.0.0.0", 12345))
+server.listen(2)
 clients = []
 
+PLAYER_COLORS = [(255, 0, 0), (0, 0, 255)]  # Rouge et Bleu
 
-def handle_client(client_socket):
+def handle_client(client_socket, player_color):
     while True:
         try:
             data = client_socket.recv(1024)
@@ -17,8 +18,8 @@ def handle_client(client_socket):
                 print(f"Client {client_socket.getpeername()} a quitté le jeu.")
                 clients.remove(client_socket)
                 client_socket.close()
-            else : 
-                broadcast(data, client_socket)
+            else:
+                broadcast(data, client_socket, player_color)
 
         except Exception as e:
             print(f"Erreur: {str(e)}")
@@ -26,22 +27,25 @@ def handle_client(client_socket):
 
     client_socket.close()
 
-
-def broadcast(message, client_socket):
+def broadcast(message, client_socket, player_color):
     for client in clients:
         if client != client_socket:
             try:
-                client.send(message)
+                # Envoyer les coordonnées x et y ainsi que la couleur du joueur
+                client.send(f"{message.decode()},{player_color}".encode())
             except Exception as e:
                 print(f"Erreur lors de la diffusion: {str(e)}")
 
 print("Le serveur est prêt à écouter les connexions...")
+player_index = 0
 while True:
     client_socket, addr = server.accept()
     clients.append(client_socket)
     print(f"Client {addr} connecté au serveur.")
 
-    client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+    # Assigner la couleur du joueur actuel
+    player_color = PLAYER_COLORS[player_index]
+    player_index = (player_index + 1) % len(PLAYER_COLORS)
+
+    client_handler = threading.Thread(target=handle_client, args=(client_socket, player_color))
     client_handler.start()
-
-
