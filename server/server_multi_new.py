4c8dac8e611093c20c2d8dffb237082bd17d6380
ElessarWillomoont <xynmpsy7ihm5mv22ode@outlook.com>
2023-10-13 10:31:50 +0200

    debug the server

--- server/server_multi_new.py
+++ server/server_multi_new.py
@@ -6,7 +6,7 @@ from multiprocessing import Process, Manager, Lock
 #some print used to debug and test
 
 # Server constants
-SERVER_IP = "127.0.0.1"
+SERVER_IP = "172.21.72.240"
 SERVER_PORT = 6859
 ALLOWED_USERNAMES = ["1111", "2222", "3333", "4444"]
 LOGIN_SUC = "login_success"
@@ -70,7 +70,7 @@ def handle_client(client_socket, game_grid, player_positions,allocated_codes,loc
                 client_socket.sendall("waiting".encode('utf-8'))
                 #listen the control code, wether to start
                 while True:
-                    print(shared_Char)
+                    #print(shared_Char)
                     if shared_Char.value == START:
                         break
                     time.sleep(0.1)
@@ -85,11 +85,10 @@ def handle_client(client_socket, game_grid, player_positions,allocated_codes,loc
                         move_command = client_socket.recv(1024).decode('utf-8').split(',')
                         player_code, direction = move_command[0], move_command[1]
                         x, y = player_positions[player_code]
+                        print(player_positions[player_code])
+                        grid_current_x, grid_current_y = (x - 100) // GRID_SIZE, y // GRID_SIZE
                         with lock:
                             old_place[player_code] = player_positions[player_code]
-
-                        grid_x_current_maunal, grid_y_current_manual = (x - 100) // GRID_SIZE, y // GRID_SIZE
-                        game_grid[grid_y_current_manual][grid_x_current_maunal] = "unused"
 
                         if direction == "up" and y > 0:
                             y -= GRID_SIZE
@@ -99,11 +98,21 @@ def handle_client(client_socket, game_grid, player_positions,allocated_codes,loc
                             x -= GRID_SIZE
                         elif direction == "right" and x < 670:
                             x += GRID_SIZE
-
-                        game_grid[grid_y_current_manual][grid_y_current_manual] = "used"
+                        print(game_grid)
+                        grid_x, grid_y = (x - 100) // GRID_SIZE, y // GRID_SIZE
+                        if game_grid[grid_y][grid_x] == "used":
+                            print([player_code],"loss at",[index])
+                            client_socket.sendall(f"{player_code},loss".encode('utf-8'))
+                        else:
+                            player_positions[player_code]=(x,y)
+                            game_grid[grid_current_y][grid_current_x] = "used"
+                        
+                        #below code for check wether the grid point is used
+                        '''
+                        #game_grid[grid_y_current_manual][grid_y_current_manual] = "used"
 
                         # Check if the grid cell is used
-                        grid_x, grid_y = (x - 100) // GRID_SIZE, y // GRID_SIZE
+                        
                         if game_grid[grid_y][grid_x] == "used":
                             client_socket.sendall(f"{player_code},loss".encode('utf-8'))
                             #break  # Exit the loop, ending the game for this player
@@ -118,14 +127,17 @@ def handle_client(client_socket, game_grid, player_positions,allocated_codes,loc
                         delta_y = new_place[player_code][1] - old_place[player_code][1]
                         with lock:
                             delta_place[player_code] = (delta_x, delta_y)
+                        '''
                     except socket.timeout:
+                        #below code for auto move
+                        '''
                         #print("No data received after 0.1 seconds, moving on...")
                         for player_code in PLAYER_CODES:
 
                             x0,y0 = player_positions[player_code]
                             # Before processing the auto move, free up their current position
-                            grid_x_current, grid_y_current = (x0 - 100) // GRID_SIZE, y0 // GRID_SIZE
-                            game_grid[grid_y_current][grid_x_current] = "unused"
+                            #grid_x_current, grid_y_current = (x0 - 100) // GRID_SIZE, y0 // GRID_SIZE
+                            #game_grid[grid_y_current][grid_x_current] = "unused"
 
                             delta_x,delta_y = delta_place[player_code]
                             x1 = x0 + delta_x*SPEED
@@ -134,7 +146,7 @@ def handle_client(client_socket, game_grid, player_positions,allocated_codes,loc
                                 player_positions[player_code] = (x1,y1)
 
                             # After moving the player, mark their new position as used
-                            game_grid[grid_y_current][grid_x_current] = "used"
+                            #game_grid[grid_y_current][grid_x_current] = "used"
 
                             #print(x1,x0,y1,y0)
                             if (delta_x !=0 or delta_y != 0):
@@ -147,6 +159,7 @@ def handle_client(client_socket, game_grid, player_positions,allocated_codes,loc
                                     #break  # Exit the loop, ending the game for this player
                                 else:
                                     game_grid[grid_y_delta][grid_x_delta] = "used"
+                        '''
                     positions = ','.join([f"{code}:{x}-{y}" for code, (x, y) in player_positions.items()])
                     client_socket.sendall(positions.encode('utf-8'))
 
